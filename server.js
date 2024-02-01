205e02ba26a1c826ede15ffeefc82f11ff0d6084
Kavin Dail <kavindail17@gmail.com>
2024-01-31 17:47:49 -0800

    redeploy

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 const { URLSearchParams } = require('url');
-const mo = require('./modules/utils')
 
-const httpInstance = require('http');
-const portNumber = 8080;
+const mo = require('../modules/utils');
 
-const httpServer = httpInstance.createServer((req, res) => {
-    const queryString = window.location.search; 
-    const URLparams = new URLSearchParams(queryString);
-    const name = URLparams.get(name);
+module.exports = (req, res) => {
+    const URLparams = new URLSearchParams(req.url.replace('/', ''));
+    const name = URLparams.get('name');
 
     res.setHeader('Content-Type', 'text/html');
-    res.write('Response from server');
-    res.write(`response was ${name}`);
+    res.write('Response from server\n');
+    res.write(`response was ${name}\n`);
     res.write(mo.getInfo());
-    res.statusCode = 200;
-
     res.end();
-});
-
-httpServer.listen(portNumber, () => {
-    console.log('Server is listening on port ' + portNumber);
-});
+};
